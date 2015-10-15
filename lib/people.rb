7379861b722b4fe599cdaa26e6f6df16296f7b7e
David Judd <david@academia.edu>
2015-10-15 12:32:09 -0700

    Remove duplicate keys

--- lib/people.rb
+++ lib/people.rb
@@ -266,9 +266,6 @@ module People
         :parsed      => false,
         :parse_type  => "",
 
-        :parsed      => false,
-        :parse_type  => "",
-
         :parsed2     => false,
         :parse_type2 => "",
 
